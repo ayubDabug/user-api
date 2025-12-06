e128fcbcd12862261962ecd02d91701de49f949d
Ayub Abdelgawad <ayubabdelgawad@gmail.com>
2025-12-05 21:37:11 -0500

    Update server.js

--- api/server.js
+++ api/server.js
@@ -18,7 +18,6 @@ jwtOptions.secretOrKey = process.env.JWT_SECRET;
 
 // Define the Strategy
 const strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
-    console.log('payload received', jwt_payload);
 
     if (jwt_payload) {
         // The following will ensure that all routes using 
@@ -144,6 +143,7 @@ app.get("/", (req, res) => {
 
 
 module.exports = app;
+
 
 
 
